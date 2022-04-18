4eb7ee3a299efc71eb039ef27a6486bb39a45fec
George Vorobyov <w53475@student.wsiz.rzeszow.pl>
2022-04-18 21:30:13 +0200

    init static API

---
+++ src/assets/api/cities.ts
@@ -0,0 +1,32 @@
+export const cities = [
+  {
+    name: 'Warsaw',
+    iso2: 'PL',
+    long: 21.01223,
+    lat: 52.229675,
+  },
+  {
+    name: 'Madrid',
+    iso2: 'ES',
+    long: -3.70379,
+    lat: 40.416775,
+  },
+  {
+    name: 'Kiev',
+    iso2: 'UA',
+    long: 30.523399,
+    lat: 50.4501,
+  },
+  {
+    name: 'Berlin',
+    iso2: 'DE',
+    long: 13.404954,
+    lat: 52.520008,
+  },
+  {
+    name: 'Lisbon',
+    iso2: 'PT',
+    long: -9.142685,
+    lat: 38.736946,
+  },
+];
